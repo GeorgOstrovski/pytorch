720cbdee1d030e822d10c31f5779aeae3b7f6c14
Supriya Rao <supriyar@fb.com>
2020-09-29 15:26:12 -0700

    [quant] Make choose_qparams_optimized return Tensors to preserve dtype
    Summary:
    Returning double values requires special handling as a return type for aten functions.
    Instead return tensors where the type is preserved in the tensor dtype
    
    Test Plan:
    python test/test_quantization.py TestQuantizedTensor.test_choose_qparams_optimized
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 807667faa1012387a82ef710ea63dc833071073c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/45530

--- aten/src/ATen/native/quantized/QTensor.cpp
+++ aten/src/ATen/native/quantized/QTensor.cpp
@@ -248,12 +248,11 @@ float calculate_quant_loss(
   float inverse_scale = 1.0f / scale;
 
   float norm = 0.0f;
-  constexpr int VLEN = 8;
   int i = 0;
 
-// TODO add FBGEMM kernel
-// #ifdef USE_FBGEMM
-// #endif
+  // TODO add FBGEMM kernel
+  // #ifdef USE_FBGEMM
+  // #endif
 
   // remainder loop
   for (; i < numel; i++) {
@@ -271,7 +270,7 @@ float calculate_quant_loss(
   and tries to minimize the quant error by doing `torch.norm(x-fake_quant(x,s,z))`
   Returns the optimized xmax and xmin value of the tensor.
 */
-std::tuple<double, double> choose_qparams_optimized(
+std::tuple<Tensor, Tensor> choose_qparams_optimized(
     const at::Tensor& input_tensor,
     int64_t numel,
     const int64_t n_bins,
@@ -318,7 +317,11 @@ std::tuple<double, double> choose_qparams_optimized(
     }
   }
 
-  return std::make_tuple((float) xmax, (float) xmin);
+  at::Tensor xmax_tensor = at::empty({1});
+  at::Tensor xmin_tensor = at::empty({1});
+  xmax_tensor[0] = xmax;
+  xmin_tensor[0] = xmin;
+  return std::make_tuple(xmax_tensor, xmin_tensor);
 }
 } // namespace native
 } // namespace at
